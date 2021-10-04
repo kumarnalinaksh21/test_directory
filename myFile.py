9e2685185c6f18e178fa18bf294e05251a316a0f
Kumar Nalinaksh <kumarnalinaksh21@gmail.com>
2021-10-04 14:06:39 +0530

    Initial commit

---
+++ myFile.py
@@ -0,0 +1,25 @@
+import sys
+import json
+
+n = len(sys.argv) #total arguments passed to the script
+m = ""
+c = 0
+
+def CheckInputAndConsolidateMessage(): 
+    global n, m, c
+
+    for i in range(1, n): #Forming message from the argument
+        m = m + sys.argv[i] + " "
+
+    for i in m: #counting number of characters in the message
+        c = c + 1
+    c = c - 1 #removing the count for the space character at the end of the message
+
+    myDict = { "message" : m, "characters" : c}
+    myDict = json.dumps(myDict, indent=4)
+    print(myDict)
+    return myDict
+    
+
+
+CheckInputAndConsolidateMessage()
